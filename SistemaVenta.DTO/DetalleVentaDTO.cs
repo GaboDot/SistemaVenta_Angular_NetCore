625d8d56a7274eff0e3e27acd62982ba9c556f47
GaboDot <gfloresolea@outlook.com>
2023-06-22 18:03:57 -0600

    Creation of all DTO's classes for interaction with frontend layer

---
+++ SistemaVenta.DTO/DetalleVentaDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.DTO
+{
+    public class DetalleVentaDTO
+    {
+        public int? IdProducto { get; set; }
+
+        public string? DescripcionProducto { get; set; }
+
+        public int? Cantidad { get; set; }
+
+        public string? PrecioText { get; set; }
+
+        public string? TotalText { get; set; }
+    }
+}
