75fce4572f3abdf531b093910d6db7d8bd550439
GaboDot <gfloresolea@outlook.com>
2023-06-26 16:44:44 -0600

    Added dependecies of services-contracts that were created.

--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -5,6 +5,8 @@ using SistemaVenta.DAL.DBContext;
 using SistemaVenta.DAL.Repositorios.Contrato;
 using SistemaVenta.DAL.Repositorios;
 using SistemaVenta.Utilities;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.BLL.Servicios;
 
 namespace SistemaVenta.IOC
 {
@@ -21,6 +23,14 @@ namespace SistemaVenta.IOC
             service.AddScoped<IVentaRepository, VentaRepository>();
 
             service.AddAutoMapper(typeof(AutoMapperProfile));
+
+            service.AddScoped<IRolService, RolService>();
+            service.AddScoped<IUsuarioService, UsuarioService>();
+            service.AddScoped<ICategoriasService, CategoriaService>();
+            service.AddScoped<IProductoService, ProductoService>();
+            service.AddScoped<IVentaService, VentaService>();
+            service.AddScoped<IDashboardService, DashboardService>();
+            service.AddScoped<IMenuService, MenuService>();
         }
     }
 }
