2caf5fa83fa5b10554ed92b0f02c61760b666097
GaboDot <gfloresolea@outlook.com>
2023-07-03 15:55:49 -0600

    Creation of Utility service for showing alerts, set, retrieve and remove sesion information.

--- AppAngular/src/app/reusable/utility.service.ts
+++ AppAngular/src/app/reusable/utility.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { Sesion } from '../interfaces/sesion';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilityService {
 
-  constructor() { }
+  constructor(private _snackBar: MatSnackBar) { }
+
+  mostrarAlerta(mensaje: string, tipo: string) {
+    this._snackBar.open(mensaje, tipo, {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 3000
+    });
+  }
+
+  guardarSesionUsuario(usuarioSesion: Sesion) {
+    localStorage.setItem('usuario', JSON.stringify(usuarioSesion));
+  }
+
+  obtenerSesionUsuario() {
+    const dataCadena = localStorage.getItem('usuario');
+    const usuario = JSON.parse(dataCadena!);
+    return usuario;
+  }
+
+  eliminarSesion() {
+    localStorage.removeItem('usuario');
+  }
+
 }
