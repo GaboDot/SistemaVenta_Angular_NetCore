bd7e86e95ee9a075d628d81b2bb0e3e55d736d08
GaboDot <gfloresolea@outlook.com>
2023-07-03 15:20:07 -0600

    Configuring routing for all pages

--- AppAngular/src/app/components/layout/layout-routing.module.ts
+++ AppAngular/src/app/components/layout/layout-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UsuarioComponent } from './pages/usuario/usuario.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+import { VentaComponent } from './pages/venta/venta.component';
+import { HistorialVentaComponent } from './pages/historial-venta/historial-venta.component';
+import { ReporteComponent } from './pages/reporte/reporte.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: LayoutComponent, 
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'usuarios', component: UsuarioComponent },
+      { path: 'productos', component: ProductoComponent },
+      { path: 'venta', component: VentaComponent },
+      { path: 'historial_venta', component: HistorialVentaComponent },
+      { path: 'reportes', component: ReporteComponent }
+    ]
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
