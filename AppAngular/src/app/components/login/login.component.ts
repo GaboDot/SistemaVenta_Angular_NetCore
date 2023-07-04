ab03a0b07102a5338c2e420cb12ab9980e7a692f
GaboDot <gfloresolea@outlook.com>
2023-07-04 14:11:22 -0600

    Created Login layout and connection with api for success.

--- AppAngular/src/app/components/login/login.component.ts
+++ AppAngular/src/app/components/login/login.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
+import { Login } from 'src/app/interfaces/login';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { UtilityService } from 'src/app/reusable/utility.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  formularioLogin: FormGroup;
+  ocultarPassword: boolean = true;
+  mostrarLoading: boolean = false;
+
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private _usuarioService : UsuarioService,
+    private _utilityService: UtilityService
+  ) {
+    this.formularioLogin = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  iniciarSesion() {
+    this.mostrarLoading = true;
+    const request: Login = {
+      correo: this.formularioLogin.value.email,
+      clave: this.formularioLogin.value.password
+    }
+    this._usuarioService.iniciarSesion(request).subscribe({
+      next: (data) => {
+        if(data.status) {
+          this._utilityService.guardarSesionUsuario(data.value);
+          this.router.navigate(['pages']);
+        }
+        else {
+          this._utilityService.mostrarAlerta('Usuario o clave incorrectos', 'Cerrar');
+        }
+      },
+      complete: () => {
+        this.mostrarLoading = false;
+      },
+      error: () => {
+        this._utilityService.mostrarAlerta('Error al iniciar sesión', 'Cerrar');
+      }
+    });
+  }
 }
--- SistemaVenta.DTO/SesionDTO.cs
+++ SistemaVenta.DTO/SesionDTO.cs
@@ -14,6 +14,6 @@ namespace SistemaVenta.DTO
 
         public string? Correo { get; set; }
 
-        public int? RolDescripcion { get; set; }
+        public string? RolDescripcion { get; set; }
     }
 }
